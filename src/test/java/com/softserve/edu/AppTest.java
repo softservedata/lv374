e4479e0fbab4b803bae6cb8d84b5b744f1fb6e25
Milnik322 <milniklalka07@gmail.com>
2019-01-28 13:52:57 +0200

    Added first test for wishlist

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -5,10 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -21,51 +19,48 @@ public class AppTest {
 		Assert.assertTrue(true);
 	}
 
-	@Test
-	public void testApp1() throws Exception {
-		System.out.println("surefire.reports.directory = " + System.getProperty("surefire.reports.directory"));
-		System.out.println("selenium.version = " + System.getProperty("selenium.version"));
-		System.out.println("System.getenv().database.password = " + System.getenv().get("DATABASE_PASSWORD"));
-	}
-	
-//	@DataProvider(parallel = true)
-//	public Object[][] concurrencyData() {
-//		return new Object[][] {
-//			{ "1" },
-//			{ "2" },
-//		};
+//	@Test
+//	public void testApp1() throws Exception {
+//		System.out.println("surefire.reports.directory = " + System.getProperty("surefire.reports.directory"));
+//		System.out.println("selenium.version = " + System.getProperty("selenium.version"));
+//		System.out.println("System.getenv().database.password = " + System.getenv().get("DATABASE_PASSWORD"));
 //	}
 
+	/**
+	 * Positive test.
+	 * User technique: State transition.
+	 * This method check if right goods added to wish list.
+	 */
 	@Test//(dataProvider = "concurrencyData")
 	public void checkSeleniumIDE() throws Exception {
-//		System.setProperty("webdriver.chrome.driver",
-//				"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
-		System.out.println("this.getClass().getResource(\"/chromedriver-windows-32bit.exe\").getPath() = "
-				+ this.getClass().getResource("/chromedriver-windows-32bit.exe").getPath());
 		System.setProperty("webdriver.chrome.driver",
-				this.getClass().getResource("/chromedriver-windows-32bit.exe").getPath().substring(1));
+				"/home/milnik/SoftServe/GitProject/lv374/target/test-classes/chromedriver-linux-64bit");
+//		System.out.println("this.getClass().getResource(\"/chromedriver-linux-64bit\").getPath() = "
+//				+ this.getClass().getResource("//chromedriver-linux-64bit").getPath());
+//		System.setProperty("webdriver.chrome.driver",
+//				this.getClass().getResource("//chromedriver-linux-64bit").getPath().substring(1));
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		//
-		driver.get("https://www.google.com");
-		// driver.navigate().to("https://www.google.com");
-		Thread.sleep(1000); // For Presentation Only
+		driver.get("http://taqc-opencart.epizy.com/index.php?route=common/home");
 		//
-		driver.findElement(By.name("q")).click();
-		driver.findElement(By.name("q")).clear();
-		driver.findElement(By.name("q")).sendKeys("selenium automation" + Keys.ENTER);
-		Thread.sleep(1000); // For Presentation Only
+		driver.findElement(By.xpath("//a[@title='My Account']")).click();
 		//
-		driver.findElement(By.xpath("//h3[text()='Selenium - Web Browser Automation']/..")).click();
-		Thread.sleep(1000); // For Presentation Only
+		driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
 		//
-		driver.findElement(By.linkText("Download")).click();
-		Thread.sleep(1000); // For Presentation Only
+		driver.findElement(By.xpath("//*[@id='input-email']")).sendKeys("milniklalka07@gmaik.com");
 		//
-		String expected = "The Selenium Server is needed in order to run Remote Selenium WebDriver. Selenium 3.X is no longer capable of running Selenium RC directly, rather it does it through emulation and the WebDriverBackedSelenium interface.";
-		String actual = driver.findElement(By.xpath("//h3[text()='Selenium Standalone Server']/following-sibling::p[1]")).getText();
+		driver.findElement(By.xpath("//*[@id='input-password']")).sendKeys("krappa123" + Keys.ENTER);
+		//
+		driver.findElement(By.xpath("//*[@id='logo']/a/img")).click();
+		//
+		driver.findElement(By.cssSelector("#content > div.row > div:nth-child(2) > div > div.button-group > button:nth-child(2)")).click();
+		//
+		driver.findElement(By.xpath("//*[@id=\'wishlist-total\']")).click();
+		//
+		String expected = "iPhone 3";
+		String actual = driver.findElement(By.cssSelector("#content > div.table-responsive > table > tbody > tr > td:nth-child(2) > a")).getText();
 		Assert.assertEquals(actual, expected, "My_Error");
-		Thread.sleep(4000); // For Presentation Only
 		//
 		driver.quit();
 	}
