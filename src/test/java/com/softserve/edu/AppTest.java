21c1eb7b83debb4ce7d591bea9d64636d74edc74
Strixee <makskondratiuk@gmail.com>
2019-02-04 15:17:20 +0200

    ver. 2.0 add 4 tests

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -8,48 +8,79 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for Opencart.
  */
 public class AppTest {
+	WebDriver driver;
 
-	//@Test
-	public void testApp() {
-		Assert.assertTrue(true);
-	}
-
-	@Test
-	public void testApp1() throws Exception {
-		System.out.println("surefire.reports.directory = " + System.getProperty("surefire.reports.directory"));
-		System.out.println("selenium.version = " + System.getProperty("selenium.version"));
-		System.out.println("System.getenv().database.password = " + System.getenv().get("DATABASE_PASSWORD"));
-	}
-	
-	@Test
-	public void checkSeleniumIDE() throws Exception {
-		System.out.println("this.getClass().getResource(\"/chromedriver-windows-32bit.exe\").getPath() = "
-				+ this.getClass().getResource("/chromedriver-windows-32bit.exe").getPath());
+	@BeforeClass
+	public void DriverSetup() {
 		System.setProperty("webdriver.chrome.driver",
 				this.getClass().getResource("/chromedriver-windows-32bit.exe").getPath().substring(1));
-		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("http://taqc-opencart.epizy.com");
-		Thread.sleep(1000);
-		//
-		driver.findElement(By.name("search")).click();
-		driver.findElement(By.name("search")).clear();
-		driver.findElement(By.name("search")).sendKeys("IPhone 3" + Keys.ENTER);
-		Thread.sleep(1000); // For Presentation Only
-		//
-		String expectd = "The Seleenium Server is needed in order to run Remote Selenium WebDriver. Selenium 3.X is no longer capable of running Selenium RC directly, rather it does it through emulation and the WebDriverBackedSelenium interface.";
-		String actual = driver.findElement(By.xpath("//h3[text()='Selenium Standalone Server']/following-sibling::p[1]")).getText();
-		Assert.assertEquals(actual, expected, "My_Error");
-		Thread.sleep(4000); // For Presentation Only
-		//
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+
+	@AfterClass
+	public void afterClass() {
 		driver.quit();
 	}
 
+	@BeforeMethod
+	public void GoToMain() {
+		driver.get("http://localhost/index.php");
+	}
+
+	@Test
+	public void checkSeleniumIDE() throws Exception {
+		driver.findElement(By.name("search")).click();
+		driver.findElement(By.name("search")).clear();
+		driver.findElement(By.name("search")).sendKeys("iPhone" + Keys.ENTER);
+		String expected = "iPhone";
+		String actual = driver
+				.findElement(
+						By.xpath("//div[contains(@class,'product-layout product-grid')]//a[contains(text(),'iPhone')]"))
+				.getText();
+		Assert.assertEquals(actual, expected, "My_Error");
+	}
+
+	@Test
+	public void checkSeleniumIDE2() throws Exception {
+		driver.findElement(By.name("search")).click();
+		driver.findElement(By.name("search")).clear();
+		driver.findElement(By.name("search")).sendKeys("Xiaomi" + Keys.ENTER);
+
+		String expected = "There is no product that matches the search criteria.";
+		String actual = driver.findElement(By.cssSelector("#content > p:nth-child(7)")).getText();
+		Assert.assertEquals(actual, expected, "My_Error");
+	}
+
+	@Test
+	public void checkSeleniumIDE3() throws Exception {
+		driver.findElement(By.name("search")).click();
+		driver.findElement(By.name("search")).clear();
+		driver.findElement(By.name("search")).sendKeys("Samsung" + Keys.ENTER);
+		driver.findElement(By.xpath("//select[@id='input-sort']")).click();
+		driver.findElement(By.xpath("//*[@id='input-sort']/option[5]")).click();
+		String expected = "Ex Tax: $200.00";
+		String actual = driver.findElement(By.xpath("//span[@class='price-tax']")).getText();
+		Assert.assertEquals(actual, expected, "My_Error");
+	}
+
+	@Test
+	public void checkSeleniumIDE4() throws Exception {
+		driver.findElement(By.name("search")).click();
+		driver.findElement(By.name("search")).clear();
+		driver.findElement(By.name("search")).sendKeys("samsung" + Keys.ENTER);
+		String expected = "samsung";
+		String actual = driver.findElement(By.xpath("//input[@value='samsung']")).getText();
+		Assert.assertEquals(actual, expected, "My_Error");
+	}
 }
