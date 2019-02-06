81c7c3d25b00716ca93e984413ca96c04ccacf5c
Strixee <makskondratiuk@gmail.com>
2019-02-06 13:26:44 +0200

    add comments

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -1,7 +1,7 @@
 package com.softserve.edu;
 
 import java.util.concurrent.TimeUnit;
-
+import java.util.ArrayList;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -44,11 +44,8 @@ public class AppTest {
 		driver.findElement(By.name("search")).clear();
 		driver.findElement(By.name("search")).sendKeys("iPhone" + Keys.ENTER);
 		String expected = "iPhone";
-		String actual = driver
-				.findElement(
-						By.xpath("//div[contains(@class,'product-layout product-grid')]//a[contains(text(),'iPhone')]"))
-				.getText();
-		Assert.assertEquals(actual, expected, "My_Error");
+		String actual = driver.findElement(By.xpath("//div[contains(@class,'product-layout product-grid')]//a[contains(text(),'iPhone')]")).getText();
+		Assert.assertEquals(actual, expected, "Wrong search result");
 	}
 
 	@Test
@@ -58,10 +55,10 @@ public class AppTest {
 		driver.findElement(By.name("search")).sendKeys("Xiaomi" + Keys.ENTER);
 
 		String expected = "There is no product that matches the search criteria.";
-		String actual = driver.findElement(By.cssSelector("#content > p:nth-child(7)")).getText();
-		Assert.assertEquals(actual, expected, "My_Error");
+		String actual = driver.findElement(By.xpath("//*[@id='content']/p[2]")).getText();
+		Assert.assertEquals(actual, expected, "Missing error message");
+	
 	}
-
 	@Test
 	public void checkSeleniumIDE3() throws Exception {
 		driver.findElement(By.name("search")).click();
@@ -71,7 +68,7 @@ public class AppTest {
 		driver.findElement(By.xpath("//*[@id='input-sort']/option[5]")).click();
 		String expected = "Ex Tax: $200.00";
 		String actual = driver.findElement(By.xpath("//span[@class='price-tax']")).getText();
-		Assert.assertEquals(actual, expected, "My_Error");
+		Assert.assertEquals(actual, expected, "Wrong filter work");
 	}
 
 	@Test
@@ -80,7 +77,35 @@ public class AppTest {
 		driver.findElement(By.name("search")).clear();
 		driver.findElement(By.name("search")).sendKeys("samsung" + Keys.ENTER);
 		String expected = "samsung";
-		String actual = driver.findElement(By.xpath("//input[@value='samsung']")).getText();
-		Assert.assertEquals(actual, expected, "My_Error");
+		String actual = driver.findElement(By.xpath("//*[@id='input-search']")).getAttribute("value");
+		Assert.assertEquals(actual, expected, "Wrong keyword");
+	}
+	
+	@Test
+	public void checkSeleniumIDE5() throws Exception {
+		driver.findElement(By.name("search")).click();
+		driver.findElement(By.name("search")).clear();
+		driver.findElement(By.name("search")).sendKeys("%" + Keys.ENTER);
+		String expected = "There is no product that matches the search criteria.";
+		String actual = driver.findElement(By.xpath("//*[@id='content']/p[2]")).getText();
+		Assert.assertEquals(actual, expected, "Wrong characters entered");
+	}
+	@Test
+	public void checkSeleniumIDE6() throws Exception {
+		driver.findElement(By.name("search")).click();
+		driver.findElement(By.name("search")).clear();
+		driver.findElement(By.name("search")).sendKeys("%" + Keys.ENTER);
+		ArrayList<String> ExpectedProductList = new ArrayList<String>(); 
+		ExpectedProductList.add("iPod Classic");
+		ExpectedProductList.add("iPod Nano");
+		ExpectedProductList.add("iPod Shuffle");
+		ExpectedProductList.add("iPod Touch");
+		ArrayList<String> ActualProductList = new ArrayList<String>(); 
+		ActualProductList.add(driver.findElement(By.xpath("//div[contains(@class,'product-layout product-grid')]//a[contains(text(),'iPod Classic')]")).getText());
+		ActualProductList.add(driver.findElement(By.xpath("//div[contains(@class,'product-layout product-grid')]//a[contains(text(),'iPod Nano')]")).getText());
+		ActualProductList.add(driver.findElement(By.xpath("//div[contains(@class,'product-layout product-grid')]//a[contains(text(),'iPod Shuffle')]")).getText());
+		ActualProductList.add(driver.findElement(By.xpath("//div[contains(@class,'product-layout product-grid')]//a[contains(text(),'iPod Touch')]")).getText());
+		Assert.assertEquals(ActualProductList, ExpectedProductList, "Wrong characters entered");
 	}
 }
+
