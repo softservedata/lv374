b7c33a06e5ec6716bffd813ef3b5727f99c70503
Yaroslav Klymovets <yaroslav.klymovets@gmail.com>
2019-02-21 16:11:46 +0200

    added letter component

--- src/test/java/com/softserve/edu/mailbox/data/LetterComponent.java
+++ src/test/java/com/softserve/edu/mailbox/data/LetterComponent.java
@@ -6,23 +6,25 @@ import org.openqa.selenium.WebElement;
 public class LetterComponent {
 
 	private WebElement letterLayout;
-	
+
 	protected LetterComponent(WebElement leterLayout) {
 		this.letterLayout = leterLayout;
 	}
-	
+
+	// pageobject
+
 	public WebElement getLetterCheckBox() {
 		return letterLayout.findElement(By.xpath("//tr//td//label"));
 	}
-	
+
 	public boolean isLetterCheckBoxChecked() {
 		return getLetterCheckBox().isSelected();
 	}
-	
+
 	public void clickLetterCheckbox() {
 		getLetterCheckBox().click();
 	}
-	
+
 	public WebElement getReadStatusMark() {
 		return letterLayout.findElement(By.xpath("//tr//td[2]//span"));
 	}
@@ -30,34 +32,37 @@ public class LetterComponent {
 	public boolean isLetterRead() {
 		return getReadStatusMark().isSelected();
 	}
-	
+
 	public void clickLetterStatusMark() {
 		getReadStatusMark().click();
 	}
-	
+
 	public WebElement getLetterAuthor() {
 		return letterLayout.findElement(By.cssSelector("span.msglist__row-address-wrap"));
 	}
-	
+
 	public String getLetterAuthorText() {
 		return getLetterAuthor().getText();
 	}
-	
+
 	public WebElement getLetterTitle() {
 		return letterLayout.findElement(By.cssSelector("a.msglist__row_href"));
 	}
-	
+
 	public String getLetterTitleText() {
 		return getLetterTitle().getText();
 	}
-	
+
 	public WebElement getLetterDate() {
 		return letterLayout.findElement(By.cssSelector("td.msglist__row-date"));
 	}
-	
+
 	public String getLetterDateText() {
 		return getLetterDate().getAttribute("title");
 	}
-	
-	
+
+	// functional
+
+	// Business logic
+
 }
--- src/test/java/com/softserve/edu/mailbox/data/LetterContainer.java
+++ src/test/java/com/softserve/edu/mailbox/data/LetterContainer.java
@@ -1,5 +1,32 @@
 package com.softserve.edu.mailbox.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LetterContainer {
 
+	private final static String PRODUCT_COMPONENT_XPATH_SELECTOR = "//div[@class='msglist']//tbody//tr";
+
+	protected WebDriver driver;
+
+	private List<LetterComponent> letters;
+
+	public LetterContainer(WebDriver driver) {
+		this.driver = driver;
+		initElements();
+	}
+
+	private void initElements() {
+		letters = new ArrayList<>();
+		for (WebElement element : driver.findElements(By.xpath(PRODUCT_COMPONENT_XPATH_SELECTOR))) {
+			letters.add(new LetterComponent(element));
+		}
+	}
+
+	
+	
 }
